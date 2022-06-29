fa0f8fee88f5746d48b017315004907d8683a4e7
Gino Murin <gmurin@gmail.com>
2022-06-28 21:00:08 -0400

    Implemented logging in MainActivity

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -5,10 +5,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final static String TAG = "Main Activity";
     public final int SLICES_PER_PIZZA = 8;
 
     private EditText mNumAttendEditText;
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate was called");
         //Assign field variables values from view elements
         mNumAttendEditText = findViewById(R.id.attendEditText);
         mNumPizzasTextView = findViewById(R.id.answerTextView);
@@ -30,7 +32,14 @@ public class MainActivity extends AppCompatActivity {
         String numAttendStr = mNumAttendEditText.getText().toString();
 
         //Logic for determining how many slices of pizza per person
-        int numAttend = Integer.parseInt(numAttendStr);
+        int numAttend;
+
+        try {
+            numAttend=Integer.parseInt(numAttendStr);
+        } catch (NumberFormatException ex){
+            numAttend=0;
+            Log.v(TAG, "No number entered");
+        }
         int slicesPerPerson = 0;
         int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
 
