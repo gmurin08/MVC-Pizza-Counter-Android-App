7b7f4f92b0da76ee9f56c2171ec669993c58296b
Gino Murin <gmurin@gmail.com>
2022-06-28 20:06:07 -0400

    Initial commit

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.java
@@ -0,0 +1,39 @@
+package com.zybooks.pizzaparty;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final int SLICES_PER_PIZZA = 8;
+
+    private EditText mNumAttendEditText;
+    private TextView mNumPizzasTextView;
+    private RadioGroup mHowHungryRadioGroup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mNumAttendEditText = findViewById(R.id.attendEditText);
+        mNumPizzasTextView = findViewById(R.id.answerTextView);
+        mHowHungryRadioGroup = findViewById(R.id.hungryRadioGroup);
+    }
+
+    public void calculateClick(View view) {
+        String numAttendStr = mNumAttendEditText.getText().toString();
+
+        int numAttend = Integer.parseInt(numAttendStr);
+
+        int slicesPerPerson = 0;
+        int checkedId = mHowHungryRadioGroup.getCheckedRadioButtonId();
+        int totalPizzas = (int) Math.ceil(numAttend * slicesPerPerson /
+                (double) SLICES_PER_PIZZA);
+        mNumPizzasTextView.setText("Total pizzas: " + totalPizzas);
+
+    }
+}
